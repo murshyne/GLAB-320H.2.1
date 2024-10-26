aec202c6254162dd1a495ba8d01434fbfaf95894
Murshyne <murshyne@gmail.com>
2024-10-26 02:07:24 -0400

    Committed at: Sat, Oct 26, 2024  2:07:23 AM

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,7 +1,8 @@
 import App from "../../App";
 
 function Content() {
-  return <p>This is my first React Application, wish me luck!</p>;
+  return <p>This is my first React Application, wish me luck!, "You dont need LUCK, you got this !!!"</p>;
+
 }
 
 export default Content;
