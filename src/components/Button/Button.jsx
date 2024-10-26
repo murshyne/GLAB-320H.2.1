79cb2f73d303f08454c4414bfe3600c1019bd583
Murshyne <murshyne@gmail.com>
2024-10-26 03:53:23 -0400

    Committed at: Sat, Oct 26, 2024  3:53:22 AM

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,21 @@
-function Button(props) {
-  return (
-    <button>Click for Luck</button>
-  );
+import { useState } from "react";
 
+function Button() {
+    const [message, setMessage] = useState("Wish me luck...");
+
+  const handleClick = () => {
+    setMessage((prevMessage) =>
+      prevMessage === "Wish me luck..."
+        ? "You don't need luck"
+        : "Wish me luck..."
+    );
+  };
+  return (
+    <>
+      <button onClick={handleClick}>Click for Luck</button>
+      <p>{message}</p> 
+    </>
+  );
 }
 
-export default Button;
+export default Button
